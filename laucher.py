d3123570abd73b7904748ee9ac7519cc6cd23114
joaovtk <joaovtkinter@gmail.com>
2025-05-09 19:41:49 -0300

    Test

--- laucher.py
+++ laucher.py
@@ -1,11 +1,8 @@
 import threading
 from src.main import telegram_bot
 from src.server import flask_app
-from src.ping import send_ping
-
 # Função para rodar ambos no mesmo processo
 def main():
-    send_ping()
     # Rodando Flask em um thread separado
     flask_thread = threading.Thread(target=flask_app)
     flask_thread.start()

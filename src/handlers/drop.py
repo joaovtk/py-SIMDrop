7a431ddaad666e694763c07f56d0bd2e31902b25
joaovtk <joaovtkinter@gmail.com>
2025-05-11 23:03:24 -0300

    Initial Commit

---
+++ src/handlers/drop.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from utils.database import con, cursor
+
+async def drop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    cursor.execute("DROP TABLE usuarios")
+    cursor.execute("DROP TABLE numeros")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user (
+            userid INTEGER PRIMARY KEY,
+            service TEXT DEFAULT 'None',
+            pais TEXT DEFAULT 'None',
+            saldo FLOAT DEFAULT 0.0,
+            cpf TEXT DEFAULT 'None'
+        )
+    """)
+    cursor.execute("CREATE TABLE IF NOT EXISTS numeros (userid INT, numero VARCHAR(20), code VARCHAR(20))")
+    con.commit()
